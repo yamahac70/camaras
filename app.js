0219d74b51d2ccdf8b7e650191d2730166174c50
yamahac70 <acostamaurojulian1@gmail.com>
2022-01-11 15:54:11 -0300

    addLogica

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 let numCamara=document.getElementById("numCamara");
 const addCamara=document.getElementById("addCamara")
 const checkModo=document.getElementById("modo");
+const modoTexto=document.getElementById("modoTexto");
 let numCamaraArray=[];
 const camarasDesinstaladas=[];
-
+let modo=0;
 addCamara.addEventListener("click",()=>{
-
-    if (checkModo.checked) {
-        alert("check")
-    }else{
-        alert("no check")
-    }
-
-
     if (numCamara.value !=="") {
-        
+        if (cambiarModo()) {
+            camarasDesinstaladas.push(Number(numCamara.value))
+        }else{
+            numCamaraArray.push(Number(numCamara.value))
+        }    
     }else{
         alert("Ingrese un numero de camara ");
 
     }
 
 })
+function cambioModo(a) {
+    if (checkModo.checked) {
+       
+       modoTexto.innerHTML="CAMARAS DESINSTALADAS"
+        return true;
+
+    }else{
+        modoTexto.innerHTML="CAMARAS SIN FUNCIONAR"
+        return false;
+    }
+}
